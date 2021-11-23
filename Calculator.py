e703cb306fc517bb84e9819684ed35f9d42ef315
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-23 11:12:05 +0000

    Part 2 - Read from file
    A new function is used to process the input file and get the data into an appropriate format. The main function gets an array with all the results (to manually check for any errors) and then prints the sum of the results, as requested. Refactored the calculate function to work with an array input - it would still work as expected if given sys.argv as a parameter

--- Calculator.py
+++ Calculator.py
@@ -1,32 +1,26 @@
-from re import sub
 import sys
 
-def calculator():
-    if len(sys.argv) == 4:                  #Ensure the corrent number of inputd are taken
-        operator = sys.argv[1]
-        try:                                #Ensure the operand inputs are integers
-            first_operand = int(sys.argv[2])
-            second_operand = int(sys.argv[3])
-        except:
-            print("Invalid operand(s)")
+def calculator(question_arr):              
+    operator = question_arr[1]
+    try:                                    #Ensure the operand inputs are integers
+        first_operand = int(question_arr[2])
+        second_operand = int(question_arr[3])
+    except:
+        print("Invalid operand(s)")
+        return
+    if operator == "x":
+        return multiply(first_operand, second_operand)
+    elif operator == "/":
+        if second_operand == 0:             #Prevents divide by 0 errors
+            print("Cannot divide by 0")
             return
-        if operator == "x":
-            print(multiply(first_operand, second_operand))
-        elif operator == "/":
-            if second_operand == 0:         #Prevents divide by 0 errors
-                print("Cannot divide by 0")
-                return
-            print(divide(first_operand, second_operand))
-        elif operator == "+":
-            print(add(first_operand, second_operand))
-        elif operator == "-":
-            print(subtract(first_operand, second_operand))
-        else:
-            print("Invalid operator")
+        return divide(first_operand, second_operand)
+    elif operator == "+":
+        return add(first_operand, second_operand)
+    elif operator == "-":
+        return subtract(first_operand, second_operand)
     else:
-        print("Incorrect number of arguments")
-
-
+        print("Invalid operator")           #Only accepts valid operators
 
 def multiply(x, y):
     return x * y
@@ -40,5 +34,26 @@ def add(x, y):
 def subtract(x,y):
     return x - y
 
+def get_input_from_file(path):
+    try:
+        question_file = open(path)      #Prevents file not found errors
+    except:
+        print("Invalid file")
+        return [-1]
+    question_strings = question_file.read().splitlines()
+    question_file.close()
+    question_arr = []
+    for question in question_strings:
+        question_arr.append(question.split(" "))
+    return question_arr
+
 if __name__ == "__main__":
-    calculator()
+    questions = get_input_from_file("step_2.txt")
+    results = []
+    for question in questions:
+        if question[0] == "calc":
+            results.append(round(calculator(question), 3))
+    print("Results: ")
+    print(results)
+    print("Sum: ")
+    print(sum(results))
